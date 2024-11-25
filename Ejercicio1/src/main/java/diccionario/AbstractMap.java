7cb0461cbf1c7eabb9bfb62ae44023b178628094
Artcraft <arturoao@uma.es>
2024-11-25 13:39:25 +0100

    Fixed issue with AbstractMap boolean

--- Ejercicio1/src/main/java/diccionario/AbstractMap.java
+++ Ejercicio1/src/main/java/diccionario/AbstractMap.java
@@ -22,35 +22,35 @@ import java.util.Objects;
 
 public abstract class AbstractMap<K, V> implements Map<K, V> {
 	protected int size = 0; // Tamaño del mapa
-	
+
 	@Override
 	public boolean isEmpty() {
 		return size() == 0;
 	}
-	
+
 	@Override
 	public int size() {
 		return size;
 	}
 
-	
+
 	// Devuelve un array con los pares de clave, valor del diccionario
 	// ordenador por clave
 	protected abstract Map.Entry<K,V>[] toArray();
-	
+
 	public String toString() {
-	        return getClass().getName()+"{" +
-                "elements=" + Arrays.toString(toArray()) +
-                ", size=" + size +
-                '}';   
-	} 
+		return getClass().getName()+"{" +
+				"elements=" + Arrays.toString(toArray()) +
+				", size=" + size +
+				'}';
+	}
 
 	@Override
 	public int hashCode() {
-	  		var elements = toArray();
-        int result = Objects.hash(size);
-        result = 31 * result + Arrays.hashCode(elements);
-        return result;    	
+		var elements = toArray();
+		int result = Objects.hash(size);
+		result = 31 * result + Arrays.hashCode(elements);
+		return result;
 	}
 
 	@Override
@@ -82,6 +82,6 @@ public abstract class AbstractMap<K, V> implements Map<K, V> {
 				same = false;
 			}
 		}
-		return true;
+		return same;
 	}
 }
